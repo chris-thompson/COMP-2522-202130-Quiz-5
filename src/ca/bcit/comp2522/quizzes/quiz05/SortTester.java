5b94853aa7268434b3fecd3d0947bea5ce48f0f9
Chris <chris_thompson@bcit.ca>
2021-11-15 20:51:41 -0800

    Hello world!

---
+++ src/ca/bcit/comp2522/quizzes/quiz05/SortTester.java
@@ -0,0 +1,19 @@
+package ca.bcit.comp2522.quizzes.quiz05;
+/**
+ * SortTester profiles some sorts.
+ *
+ * @author YOUR NAME
+ * @version 202130
+ */
+public final class SortTester {
+
+    private SortTester() { }
+
+    /**
+     * Drives the program.
+     * @param args unused
+     */
+    public static void main(final String[] args) {
+
+    }
+}
